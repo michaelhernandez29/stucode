4ebc26e46e3940687a0a417388bfe5c2c6970f96
Michael Hernandez <michaelhernandezrala@gmail.com>
2024-07-03 12:43:38 +0200

    chore(api): install redoc, openapi validator and configure them

--- packages/api/src/api.js
+++ packages/api/src/api.js
@@ -4,7 +4,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const logger = require('./helpers/logger');
+const openApiValidator = require('express-openapi-validator');
+const path = require('path');
 const pinoHttp = require('pino-http');
+const redoc = require('redoc-express');
 
 require('./db/dbPostgres');
 
@@ -17,6 +20,31 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pinoHttp({ logger }));
 app.use(cors(corsOptions));
-app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'", 'unpkg.com'],
+      workerSrc: ["'self'", 'blob:'],
+    },
+  }),
+);
+app.get('/api/api.yml', (req, res) => {
+  res.sendFile('openapi/api.yml', { root: path.resolve(__dirname) });
+});
+app.use(
+  '/v1/api-docs',
+  redoc({
+    title: 'StuCode API Docs',
+    specUrl: '/api/api.yml',
+  }),
+);
+
+app.use(
+  openApiValidator.middleware({
+    apiSpec: __dirname + '/openapi/api.yml',
+    operationHandlers: __dirname + '/controllers',
+  }),
+);
 
 module.exports = app;
