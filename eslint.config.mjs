7a387b1cf84cc086a7bf5819b1eaa5ab57796243
Michael Hernandez <michaelhernandezrala@gmail.com>
2024-10-06 18:32:23 +0200

    chore: setup lerna

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
---
+++ packages/api/eslint.config.mjs
@@ -0,0 +1,49 @@
+import pluginJs from '@eslint/js';
+import pluginJsdoc from 'eslint-plugin-jsdoc';
+import pluginNode from 'eslint-plugin-node';
+import globals from 'globals';
+
+const jsdocRules = {
+  'jsdoc/require-jsdoc': [
+    'error',
+    {
+      require: {
+        FunctionDeclaration: true,
+        MethodDefinition: true,
+        ClassDeclaration: true,
+        ArrowFunctionExpression: true,
+        FunctionExpression: true,
+      },
+    },
+  ],
+  'jsdoc/require-description': 'error',
+  'jsdoc/require-param': 'error',
+  'jsdoc/require-param-type': 'error',
+  'jsdoc/require-param-description': 'error',
+  'jsdoc/require-returns': 'error',
+  'jsdoc/require-returns-type': 'error',
+  'jsdoc/require-returns-description': 'error',
+  'jsdoc/check-types': 'error',
+};
+
+const nodeRules = {
+  'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  'no-console': ['error'],
+  'node/no-unsupported-features/es-syntax': 'off',
+  'node/no-missing-import': 'off',
+};
+
+export default [
+  {
+    languageOptions: { globals: globals.node },
+    plugins: {
+      jsdoc: pluginJsdoc,
+      node: pluginNode,
+    },
+    rules: {
+      ...jsdocRules,
+      ...nodeRules,
+    },
+  },
+  pluginJs.configs.recommended,
+];
