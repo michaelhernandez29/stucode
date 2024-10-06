aaf7ab127b434f44680d80625ad839a8a14e0c77
Michael Hernandez <michaelhernandezrala@gmail.com>
2024-10-06 19:26:46 +0200

    fix: some fixes in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,36 @@ import pluginJs from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
 import globals from 'globals';
 
+const jsdocRules = {
+  'jsdoc/require-jsdoc': [
+    'error',
+    {
+      require: {
+        FunctionDeclaration: true,
+        MethodDefinition: true,
+        ClassDeclaration: true,
+        ArrowFunctionExpression: true,
+        FunctionExpression: true,
+      },
+    },
+  ],
+  'jsdoc/require-description': 'error',
+  'jsdoc/require-param': 'error',
+  'jsdoc/require-param-type': 'error',
+  'jsdoc/require-param-description': 'error',
+  'jsdoc/require-returns': 'error',
+  'jsdoc/require-returns-type': 'error',
+  'jsdoc/require-returns-description': 'error',
+  'jsdoc/check-types': 'error',
+};
+
+const nodeRules = {
+  'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  'no-console': ['error'],
+  'node/no-unsupported-features/es-syntax': 'off',
+  'node/no-missing-import': 'off',
+};
+
 export default [
   { files: ['**/*.{js,mjs,cjs,jsx}'] },
   {
@@ -12,6 +42,8 @@ export default [
   {
     rules: {
       'react/react-in-jsx-scope': 'off',
+      ...jsdocRules,
+      ...nodeRules,
     },
   },
 ];
