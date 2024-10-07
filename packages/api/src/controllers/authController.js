a351a716540567b597803b58ccfa0e05fa3f2ae3
Michael Hernandez <michaelhernandezrala@gmail.com>
2024-10-07 20:47:32 +0200

    feat(api): implement POST /users/login endpoint

--- packages/api/src/controllers/authController.js
+++ packages/api/src/controllers/authController.js
@@ -34,4 +34,35 @@ const register = async (req, res) => {
   responseHelper.created(res, response);
 };
 
-export { register };
+/**
+ * Handler for POST /users/login
+ *
+ * @param {Request} req - The Express request object.
+ * @param {Response} res - The Express response object.
+ */
+const login = async (req, res) => {
+  let payload = req.body;
+
+  if (!validator.isEmail(payload.email)) {
+    responseHelper.badRequest(res, errorMessages.INVALID_EMAIL, errorCodes.BAD_REQUEST);
+    return;
+  }
+
+  const user = await userService.findByEmail(payload.email, { raw: true });
+  if (!user) {
+    responseHelper.notFound(res, errorMessages.USER_NOT_FOUND, errorCodes.USER_NOT_FOUND);
+    return;
+  }
+
+  const isPasswordCorrect = await cryptoHelper.compare(payload.password, user.password);
+  if (!isPasswordCorrect) {
+    responseHelper.badRequest(res, errorMessages.CREDENTIALS_NOT_VALID, errorCodes.BAD_REQUEST);
+    return;
+  }
+
+  payload = _.omit(payload, 'password');
+  const data = cryptoHelper.generateToken(payload);
+  responseHelper.ok(res, data);
+};
+
+export { login, register };
